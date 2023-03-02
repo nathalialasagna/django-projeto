777e2503ba08a822979aac28543d0c0686812da8
Nathalia <nathalia.dias3010@gmail.com>
2023-03-02 13:18:12 -0300

    add html

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,8 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html', context={'name': 'NATHALIA'})
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
+    return render(request, 'recipes/pages/home.html')
