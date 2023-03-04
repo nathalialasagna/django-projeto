9c7a53e7ccc9f8da357ac0baa699f1d6d54e1940
Nathalia <nathalia.dias3010@gmail.com>
2023-03-04 00:43:48 -0300

    add adj

--- project/urls.py
+++ project/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from recipes.views import home
+from recipes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe)
 ]
