6dde2277d229d88ad44b9f5aa3bf45d83f90bb56
Nathalia <nathalia.dias3010@gmail.com>
2023-03-18 13:44:12 -0300

    model adj and url

--- recipes/models.py
+++ recipes/models.py
@@ -1,36 +1,35 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
-
-# its like a table
-
 
 class Category(models.Model):
-    name = models.CharField(max_length=60)
+    name = models.CharField(max_length=65)
 
     def __str__(self):
         return self.name
 
 
 class Recipe(models.Model):
-    title = models.CharField(max_length=60)
-    description = models.CharField(max_length=160)
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
     slug = models.SlugField()
     preparation_time = models.IntegerField()
-    preparation_time_unit = models.CharField(max_length=60)
+    preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
-    servings_time_unit = models.CharField(max_length=60)
+    servings_time_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
-    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    cover = models.ImageField(
+        upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None)
+        Category, on_delete=models.SET_NULL, null=True
+    )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True)
+        User, on_delete=models.SET_NULL, null=True
+    )
 
     def __str__(self):
         return self.title
--- recipes/views.py
+++ recipes/views.py
@@ -1,28 +1,50 @@
-from django.shortcuts import render
+# from django.http import HttpResponse
+# from django.http import Http404
+from django.shortcuts import get_list_or_404, render
 
 from recipes.models import Recipe
-from utils.recipes.factory import make_recipe
+
+# from utils.recipes.factory import make_recipe
 
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes
+        'recipes': recipes,
         # [make_recipe() for _ in range(10)]
     })
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id, is_published=True,).order_by('-id')
+
+    #    recipes = Recipe.objects.filter(
+    #        category__id=category_id, is_published=True,).order_by('-id')
+    #
+    #     category_name = getattr(
+    #        getattr(recipes.first(), 'Category', None), 'name', 'Not found')
+
+    #    if not recipes:
+    #     return HttpResponse(content='Not Found', status=404)
+    #          raise Http404('Not Found')
+
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True,).order_by('-id'))
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name}'
+        # 'title': f'{recipes.first().category.name}'
         # [make_recipe() for _ in range(10)]
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        id=id).order_by('-id').first()
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        # 'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True,
     })
