cdae5d14b3332eed65db645da5c7499db6963a55
Nathalia <nathalia.dias3010@gmail.com>
2023-03-21 01:51:56 -0300

    add tests

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 # from django.http import HttpResponse
 # from django.http import Http404
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 
@@ -8,7 +8,9 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    #    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = get_list_or_404(Recipe.objects.filter(
+        is_published=True).order_by('-id'))
 
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
@@ -29,7 +31,7 @@ def category(request, category_id):
     #          raise Http404('Not Found')
 
     recipes = get_list_or_404(Recipe.objects.filter(
-        category__id=category_id, is_published=True,).order_by('-id'))
+        category__id=category_id, is_published=True).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
@@ -40,9 +42,9 @@ def category(request, category_id):
 
 
 def recipe(request, id):
-    recipe = Recipe.objects.filter(
-        id=id).order_by('-id').first()
-
+    #    recipe = Recipe.objects.filter(
+    #         id=id).order_by('-id').first()
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True)
     return render(request, 'recipes/pages/recipe-view.html', context={
         # 'recipe': make_recipe(),
         'recipe': recipe,
